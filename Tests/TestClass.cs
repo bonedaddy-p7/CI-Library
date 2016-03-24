f3f38776448d7a02580920a6f784806874dbab99
RyanP <ryanp@javs.com>
2016-03-24 15:24:11 -0400

    Adding a Unit Test

---
+++ Tests/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void GetNowDateTimeStringTest()
+        {
+            CILib.CIStuff variable = new CILib.CIStuff();
+            string result = variable.GetNowDateTimeString();
+            Assert.False(string.IsNullOrEmpty(result.Trim()));
+        }
+    }
+}
